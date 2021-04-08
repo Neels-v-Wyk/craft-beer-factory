4b1d12813b896de6759886f59f018dbf851055e8
Neels-v-Wyk <git@neels.tech>
2021-04-08 19:37:26 +0200

    add back stuff that went missing

---
+++ src/page-creation/createNavbar.js
@@ -0,0 +1,8 @@
+function createNavbar(element) {
+    navbar = document.createElement("div")
+    navbar.classList.add("navbar")
+    element.appendChild(navbar)
+
+}
+
+export {createNavbar as default}; 
