b21c2d5d14a481cfead68ac19b010623a37135a6
Neels-v-Wyk <git@neels.tech>
2021-04-10 13:40:09 +0200

    redo the way buttons owrk

--- src/page-creation/createNavbar.js
+++ src/page-creation/createNavbar.js
@@ -7,14 +7,11 @@ function createNavbar(element) {
     var titleText = document.createElement("h1");
     titleText.innerText = "Craft Beer Restaurant"
     titleText.classList.add("titleText")
-    // addButton("stuff")
-    // addButton("Menu");
-    // addButton("Contact");
 
     navbar.appendChild(titleText);
 
     addButtons(navbar)
-    
+
     element.appendChild(navbar);
 };
 
