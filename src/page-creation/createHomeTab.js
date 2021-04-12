d49a19011238dd2fb0262a2858c0bc1d7ed5f0d7
Neels-v-Wyk <git@neels.tech>
2021-04-12 18:04:37 +0200

    use div for beerImage

--- src/page-creation/createHomeTab.js
+++ src/page-creation/createHomeTab.js
@@ -1,10 +1,20 @@
 import beerIcon from "../assets/beer.svg"
 
 export default function createHomeTab(parentElement) {
-    var beerImage = new Image();
-    console.log(beerIcon)
+    parentElement.appendChild(createBeer());
 
+}
+
+function createBeer() {
+    var beerDiv = document.createElement("div");
+    beerDiv.classList.add("beerDiv")
+
+    var beerImage = new Image();
     beerImage.src = beerIcon;
     beerImage.classList.add("beerIcon")
-    parentElement.appendChild(beerImage);
+    
+    beerDiv.appendChild(beerImage)
+
+    return beerDiv;
+
 }
