12071309be07191ebf1369af168fa38f8e5174f2
Neels-v-Wyk <git@neels.tech>
2021-04-17 19:29:05 +0200

    switch back to svg because I'm a fucking genius

--- src/page-creation/createHomeTab.js
+++ src/page-creation/createHomeTab.js
@@ -1,4 +1,4 @@
-import beerIcon from "../assets/png-beer.png"
+import beerIcon from "../assets/beer.svg"
 
 export default function createHomeTab(parentElement) {
     parentElement.appendChild(createBeer());
