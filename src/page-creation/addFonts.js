ce0d8e67848b30f5e1d1067abaf9a4635845009f
Neels-v-Wyk <git@neels.tech>
2021-04-18 21:11:56 +0200

    add intro and info

--- src/page-creation/addFonts.js
+++ src/page-creation/addFonts.js
@@ -2,7 +2,10 @@
 
 function addFonts() {
     var titleFont = document.createElement("style")
-    titleFont.innerHTML = "@import url('https://fonts.googleapis.com/css2?family=Special+Elite&display=swap');"
+    titleFont.innerHTML = `
+        @import url('https://fonts.googleapis.com/css2?family=Special+Elite&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap');
+        `
 
     document.head.appendChild(titleFont)  
 }
--- src/page-creation/createHomeTab.js
+++ src/page-creation/createHomeTab.js
@@ -2,6 +2,8 @@ import beerIcon from "../assets/beer.png"
 
 export default function createHomeTab(parentElement) {
     parentElement.appendChild(createBeer());
+    parentElement.appendChild(createIntro());
+
 
 }
 
@@ -17,4 +19,21 @@ function createBeer() {
 
     return beerDiv;
 
+}
+
+function createIntro() {
+
+    var menuTextDiv = document.createElement("div")
+    var introDiv = document.createElement("p");
+    introDiv.classList.add("intro")
+    var infoDiv = document.createElement("p");
+    infoDiv.classList.add("info")
+
+    introDiv.textContent = "Welcome to the Craft Beer Factory."
+    infoDiv.textContent = "We offer a small selection of beers, each flavor created using only our most unique blend of the industry standard ratios for hops and yeast and stuff. We brew small batches of beer with love, care, and at an industrial scale. It is our mission to make a beer so utterly average in every way that the population at large can enjoy it."
+
+    menuTextDiv.appendChild(introDiv)
+    menuTextDiv.appendChild(infoDiv)
+
+    return menuTextDiv;
 }
