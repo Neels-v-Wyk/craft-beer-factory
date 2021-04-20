0df9343d3ffdcf6149d706d8d58e95cf36b11eb3
Neels-v-Wyk <git@neels.tech>
2021-04-20 19:59:18 +0200

    clean things up a bit

--- src/page-creation/addFonts.js
+++ src/page-creation/addFonts.js
@@ -3,7 +3,7 @@
 function addFonts() {
     var titleFont = document.createElement("style")
     titleFont.innerHTML = `
-        @import url('https://fonts.googleapis.com/css2?family=Special+Elite&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap');
         `
 
