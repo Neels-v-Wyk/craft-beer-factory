628ee1bf2b4fe03fd0d2185f5e7a19e3977f115b
Neels-v-Wyk <git@neels.tech>
2021-04-28 23:32:20 +0200

    first test of beer menu

--- src/page-creation/createMenuTab.js
+++ src/page-creation/createMenuTab.js
@@ -7,8 +7,10 @@ function createMenuItems(){
 
     var noturine = new menuItem("https://i0.hippopx.com/photos/532/903/624/beer-drink-drinking-preview.jpg", "Noturiné", "$3.99")
     var sausage_juice = new menuItem("https://live.staticflickr.com/65535/48320318901_bca39d721e_k.jpg", "Sausage Juice", "$3.49")
-    var noturine = new menuItem("https://cdn.pixabay.com/photo/2017/06/20/22/43/beer-2425247_960_720.jpg", "Golden Fluid", "$2.99")
-    var lightBeer = new row()
+    var golden_fluid = new menuItem("https://cdn.pixabay.com/photo/2017/06/20/22/43/beer-2425247_960_720.jpg", "Golden Fluid", "$2.99")
+    var lightBeer = new menuRow([noturine, sausage_juice, golden_fluid], "BLONDES", "#faf0be")
+
+    menuItems.appendChild(lightBeer)
 
     return menuItems;
 }
