0254e63f8c4e3d6f3b5813cb3c7e09c34e646b4d
Neels-v-Wyk <git@neels.tech>
2021-05-16 20:41:59 +0200

    Make menu tab a bit nicer

--- src/page-creation/createMenuTab.js
+++ src/page-creation/createMenuTab.js
@@ -6,13 +6,16 @@ function createMenuItems(){
     var menuItems = document.createElement("div");
     menuItems.classList.add("menuItems")
 
-    var noturine = new menuItem("//live.staticflickr.com/4879/32531507678_821f982b1a_b.jpg", "Noturiné", "$3.99")
-    var sausage_juice = new menuItem("https://live.staticflickr.com/65535/48320318901_bca39d721e_k.jpg", "Sausage Juice", "$3.49")
-    var golden_fluid = new menuItem("https://cdn.pixabay.com/photo/2017/06/20/22/43/beer-2425247_960_720.jpg", "Golden Fluid", "$2.99")
-    var peene_sap = new menuItem("https://get.pxhere.com/photo/drink-beer-glass-beer-lager-pint-glass-pint-alcoholic-beverage-Bia-h-i-beer-stein-beer-cocktail-mug-drinkware-cider-Ice-beer-distilled-beverage-non-alcoholic-beverage-tableware-1619184.jpg", "Peene Sap", "$3.14")
+    var sausage_juice = new menuItem("https://live.staticflickr.com/65535/48320318901_bca39d721e_k.jpg", "Sausage Juice")
+    var golden_fluid = new menuItem("https://cdn.pixabay.com/photo/2017/06/20/22/43/beer-2425247_960_720.jpg", "Golden Fluid")
+    var peene_sap = new menuItem("https://get.pxhere.com/photo/drink-beer-glass-beer-lager-pint-glass-pint-alcoholic-beverage-Bia-h-i-beer-stein-beer-cocktail-mug-drinkware-cider-Ice-beer-distilled-beverage-non-alcoholic-beverage-tableware-1619184.jpg", "Peene Sap")
+    var pp_flow = new menuItem("https://www.publicdomainpictures.net/pictures/170000/velka/beer-1462977954DHo.jpg", "P.P. Flow")
+    var yellow_water = new menuItem("https://michigan-beer-growler-company-and-michigan-coffee-creamery-com.square.site/uploads/1/3/1/4/131401938/s714805473369287628_c2_i1_w600.jpeg", "Yellow Water")
+    var magnum_dew = new menuItem("https://live.staticflickr.com/65535/49643654412_9ee24d6900_z.jpg", "Magnum Dew")
+
 
     // I'm too lazy to refactor the code, the last two parameters to menuRow() are useless
-    var lightBeer = new menuRow([noturine, sausage_juice, golden_fluid, peene_sap], "BEERS YO", "#faf0be")
+    var lightBeer = new menuRow([sausage_juice, golden_fluid, peene_sap, pp_flow, yellow_water, magnum_dew], "BEERS YO", "#faf0be")
 
     menuItems.appendChild(lightBeer.addRow)
     return menuItems;
@@ -42,10 +45,9 @@ class menuRow {
 }
 
 class menuItem {
-    constructor(imageUrl, name, price) {
+    constructor(imageUrl, name) {
         this.imageUrl = imageUrl;
         this.name = name;
-        this.price = price;
     }
 
     get createItem() {
@@ -65,13 +67,9 @@ class menuItem {
         var desc = document.createElement("div");
         desc.classList.add("menuItemDescription")
 
-        var descDollars = document.createElement("p");
-        descDollars.textContent = this.name
-
         var descName = document.createElement("p");
-        descName.textContent = this.price
+        descName.textContent = this.name
 
-        desc.appendChild(descDollars)
         desc.appendChild(descName)
 
         item.appendChild(img)
